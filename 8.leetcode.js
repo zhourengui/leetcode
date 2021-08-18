2198dfe53497b67b38ae42f91d420a0c72d96ae5
ZhouRenGui <729049182@qq.com>
2021-08-18 11:41:18 +0800

    feature replace spaces

---
+++ 8.leetcode.js
@@ -0,0 +1,26 @@
+// https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/submissions/
+// 解题思路：
+// 1. 遍历，但是不能在原来的字符串中直接操作，因为字符串是线性结构
+// 时间复杂度：O(n), 空间复杂度：O(1)
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var replaceSpace = function (s) {
+  let r = "",
+    l = s.length,
+    p = 0,
+    c;
+
+  while (p < l) {
+    c = s[p];
+    if (c === " ") {
+      r += "%20";
+    } else {
+      r += c;
+    }
+    p++;
+  }
+
+  return r;
+};
