015dbc0e55921626c159c18fed23fdc9ae871442
ZhouRenGui <729049182@qq.com>
2021-09-18 21:54:36 +0800

    feature max depth

---
+++ 34.leetcode.js
@@ -0,0 +1,34 @@
+// https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+// 解题思路：
+// 1. 使用深度优先遍历
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  let maxDeepth = 0;
+
+  const dfs = (root, deepth) => {
+    if (root === null) return;
+
+    if (root.left === null && root.right === null) {
+      maxDeepth = Math.max(maxDeepth, deepth);
+    }
+
+    dfs(root.left, deepth + 1);
+    dfs(root.right, deepth + 1);
+  };
+
+  dfs(root, 1);
+
+  return maxDeepth;
+};
