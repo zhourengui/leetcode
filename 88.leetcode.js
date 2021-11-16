d2c999d104a8130163167e9d7366934fafa172be
ZhouRenGui <729049182@qq.com>
2021-11-16 20:25:38 +0800

    feature solution

--- 88.leetcode.js
+++ 88.leetcode.js
@@ -3,17 +3,34 @@
 /**
  * @param {number[]} nums
  */
-var Solution = function (nums) {};
+var Solution = function (nums) {
+  this.nums = nums;
+  this.backup = [...nums];
+};
 
 /**
  * @return {number[]}
  */
-Solution.prototype.reset = function () {};
+Solution.prototype.reset = function () {
+  this.nums = [...this.backup];
+  return this.nums;
+};
 
 /**
  * @return {number[]}
  */
-Solution.prototype.shuffle = function () {};
+Solution.prototype.shuffle = function () {
+  let len = this.nums.length;
+  while (len > 0) {
+    const random = Math.floor(Math.random() * len);
+    [this.nums[len - 1], this.nums[random]] = [
+      this.nums[random],
+      this.nums[len - 1],
+    ];
+    len--;
+  }
+  return this.nums;
+};
 
 /**
  * Your Solution object will be instantiated and called as such:
