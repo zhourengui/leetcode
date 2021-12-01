076c875fa722706a16f028caeb20c2d7a71c3b36
ZhouRenGui <729049182@qq.com>
2021-12-01 10:08:45 +0800

    update max dep

--- 96.leetcode.js
+++ 96.leetcode.js
@@ -14,23 +14,46 @@
  * @param {number[]} arr
  * @returns {number}
  */
-function maxDeep(arr) {
-  let depth = 0;
-  let stack = [...arr];
-  while (stack.length) {
-    let tmp = [];
-    depth++;
-    while (stack.length) {
-      let top = stack.pop();
-      if (Array.isArray(top)) {
-        tmp.push(...top);
-      }
-    }
-    stack = tmp;
-  }
-  return depth;
-}
+// function maxDeep(arr) {
+//   let depth = 0;
+//   let stack = [...arr];
+//   while (stack.length) {
+//     let tmp = [];
+//     depth++;
+//     while (stack.length) {
+//       let top = stack.pop();
+//       if (Array.isArray(top)) {
+//         tmp.push(...top);
+//       }
+//     }
+//     stack = tmp;
+//   }
+//   return depth;
+// }
 
 // 解法二
 // 解题思路：
 // 数左括号的数量
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+function maxDeep(arr) {
+  let p = 0;
+  let str = JSON.stringify(arr);
+  let len = str.length;
+  let depth = 0;
+  let count = 0;
+
+  while (p <= len - 1) {
+    if (str[p] === "[") {
+      count++;
+    } else if (str[p] === "]") {
+      depth = Math.max(count, depth);
+      count--;
+    }
+    p++;
+  }
+
+  return depth;
+}
