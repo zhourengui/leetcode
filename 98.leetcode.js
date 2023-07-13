9536d3ffcaf26afdb58d863fe25b6d1d863d58e7
Little Boy <729049182@qq.com>
2023-07-13 11:49:46 +0800

    Create 98.leetcode.js

---
+++ 98.leetcode.js
@@ -0,0 +1,8 @@
+// https://leetcode-cn.com/problems/compare-version-numbers/
+
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function (version1, version2) {};
